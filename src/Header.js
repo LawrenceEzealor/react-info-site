d9c38d3eb1c8099b3e14adfcfaf6bc65ebc184c2
LawrenceEzealor <ezealor20@gmail.com>
2023-09-22 18:48:48 +0100

    new commit

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Header(){
+    return (
+        <>
+            <header>
+            <h1>Lawrence</h1>
+            </header>
+            <ul>
+                <li>About</li>
+                <li>Blog</li>
+                <li>Contact</li>
+            </ul>
+        </>
+    )
+};
+
+export default Header;
