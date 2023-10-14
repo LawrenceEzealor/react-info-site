1d87c9ac5eae8be0cbfbf428401bde82a1d6142d
LawrenceEzealor <ezealor20@gmail.com>
2023-10-14 16:42:02 +0100

    new commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import logo from "./image/react-logo.png";
 function Navbar(){
     return(
        <nav>
-            <img src={logo} className="nav--icon" />
+            <img src={logo} className="nav--icon" alt="" />
             <h3 className="nav--logo_text">React Facts</h3>
             <h4 className="nav--title">React Course - Project 1</h4>
        </nav>
